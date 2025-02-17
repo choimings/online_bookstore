ff7ea4dbe030a57f504e97189d795bafd0bfd1bd
mingyeong <mingyeong775@naver.com>
2025-02-17 22:08:30 +0900

    add_env

--- back/database/database.ts
+++ back/database/database.ts
@@ -1,11 +1,19 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const sequelize = new Sequelize("online_bookstore", "postgres", "ms519", {
-  host: "localhost",
-  dialect: "postgres",
-  port: 5432,
-  logging: false, // 콘솔 로그 제거
-});
+dotenv.config(); // .env 파일 로드
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME as string,
+  process.env.DB_USER as string,
+  process.env.DB_PASSWORD as string,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    port: Number(process.env.DB_PORT) || 5432,
+    logging: false, // 콘솔 로그 제거
+  }
+);
 
 export const connectDB = async () => {
   try {
