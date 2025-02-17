17ac4b141e4a71e513c00fda7a8223f739cf8d83
mingyeong <mingyeong775@naver.com>
2025-02-18 01:41:44 +0900

    react_setting

---
+++ front/my-app/src/components/modal.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Modal = () => {
+  return <div className="modal">모달 창</div>;
+};
+
+export default Modal;
---
+++ front/my-app/src/pages/book_detail.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BookDetail: React.FC = () => {
+  return (
+    <div>
+      <h1>책 상세 페이지</h1>
+    </div>
+  );
+};
+
+export default BookDetail;
